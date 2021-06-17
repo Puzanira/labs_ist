64b5930b0e15489ed5d4fec2811774019537d80c
Puzanira <puzanova.ira2011@yandex.ru>
2021-06-17 10:59:29 +0300

    Fix

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/.netlify/functions/server', indexRouter);  // path must route to lambda
+app.use('/', indexRouter);
 
 app.use(session({
   genid: (req) => {
